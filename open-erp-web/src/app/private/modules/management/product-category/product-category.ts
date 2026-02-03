9cf7f30860cc5921891fc9e05bbb27fd939a2d9c
Vũ Văn Minh <min6th@gmail.com>
2026-02-03 10:19:57 +0700

    feat: add product-category component with template and change detection strategy

---
+++ open-erp-web/src/app/private/modules/management/product-category/product-category.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'management-product-category',
+  imports: [],
+  templateUrl: './product-category.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProductCategory { }
