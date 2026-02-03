cb47df629eae7c84c5d07da3ab9ad7325f30d8f9
Vũ Văn Minh <min6th@gmail.com>
2026-02-03 10:15:14 +0700

    feat: add product-type component with template and change detection strategy

---
+++ open-erp-web/src/app/private/modules/management/product-type/product-type.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'management-product-type',
+  imports: [],
+  templateUrl: './product-type.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProductType { }
