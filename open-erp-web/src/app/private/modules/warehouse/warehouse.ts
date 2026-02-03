7f2dc2dba3f9c3ff8ea35ab4c51fdbed5ec0b07c
Vũ Văn Minh <min6th@gmail.com>
2026-02-03 10:40:11 +0700

    feat: add warehouse module with component and template

---
+++ open-erp-web/src/app/private/modules/warehouse/warehouse.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'module-warehouse',
+  imports: [],
+  templateUrl: './warehouse.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Warehouse { }
