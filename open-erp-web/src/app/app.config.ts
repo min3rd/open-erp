19223a548fc64fc79de73a4a1a547a026902c214
Vũ Văn Minh <min6th@gmail.com>
2026-02-25 16:04:02 +0700

    feat(app.config): add dark mode selector option for PrimeNG theme

--- open-erp-web/src/app/app.config.ts
+++ open-erp-web/src/app/app.config.ts
@@ -24,6 +24,9 @@ export const appConfig: ApplicationConfig = {
     providePrimeNG({
       theme: {
         preset: Aura,
+        options: {
+          darkModeSelector: '.dark',
+        },
       },
       ripple: true,
     }),
